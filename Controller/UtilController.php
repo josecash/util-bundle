3d26a51d9d075bd6eefbfe56e6196aef74d376ac
root <root@jsc>
2015-02-21 13:33:28 +0100

    Initial commiting

---
+++ Controller/UtilController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OfCoding\UtilBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class UtilController extends Controller {
+
+    protected function doctrine() {
+        return $this->getDoctrine()->getManager();
+    }
+
+    protected function doctrineRepo($repo) {
+        return $this->getDoctrine()->getManager()->getRepository($repo);
+    }
+
+    protected function flashMsg($type, $msg) {
+        $this->get('session')->getFlashBag()->add($type, $msg);
+    }
+
+    protected function mailing($subject, $from, $to, $view) {
+        $message = \Swift_Message::newInstance();
+        $message->setSubject($subject)
+                ->setFrom($from)
+                ->setTo($to)
+                ->setBody($view, 'text/html');
+
+        $this->get('mailer')->send($message);
+    }
+
+}
---
+++ Entity/TimeUtil.php
@@ -0,0 +1,90 @@
+<?php
+namespace OfCoding\UtilBundle\Entity;
+
+class TimeUtil {
+
+    /**
+     * Pasa una fecha en formato MySQL a formato Español.
+     * 
+     * @param String $fecha
+     * @return String -- La fecha en español.
+     */
+    public static function fechaSpanish($fecha) {
+        return $fechaES;
+    }
+
+    /**
+     * Pasa una fecha en formato español a formato Ingles o MySQL.
+     *
+     * @param String $fecha
+     * @return String -- La fecha en inglés.
+     */
+    public static function fechaInglesa() {
+        return $fechaEN;
+    }
+
+    public static function fromMySQLToLocal($dateStr, $toFormat = 'd-m-Y') {
+        $date = DateTime::createFromFormat('Y-m-d', $dateStr);
+        if (!$date)
+            return "";
+        
+        return $date->format($toFormat);
+    }
+
+    public static function fromLocalToMySQL($dateStr, $fromFormat = 'd-m-Y') {
+        $date = DateTime::createFromFormat($fromFormat, $dateStr);
+        if (!$date)
+            return "";
+
+        return $date->format('Y-m-d');
+    }
+
+    public static function humanTiming($timeStr) {
+        $time = strtotime($timeStr);
+        $time = time() - $time;
+        $plurals = array('mes' => 'meses');
+        $tokens = array(
+            31536000 => 'año',
+            2592000 => 'mes',
+            604800 => 'semana',
+            86400 => 'día',
+            3600 => 'hora',
+            60 => 'minuto',
+            1 => 'segundo'
+        );
+
+        foreach ($tokens as $unit => $text) {
+            if ($time < $unit)
+                continue;
+            $numberOfUnits = floor($time / $unit);
+            if ($numberOfUnits > 1) {
+                if (array_key_exists($text, $plurals)) {
+                    $text = $plurals[$text];
+                } else {
+                    $text .= 's';
+                }
+            }
+
+            return "Hace " . $numberOfUnits . ' ' . $text;
+        }
+    }
+
+    public static function isPast($dateStr, $fromFormat = 'Y-m-d') {
+        $date = DateTime::createFromFormat($fromFormat, $dateStr);
+        $today = new DateTime("now");
+        if ($date < $today)
+            return true;
+
+        return false;
+    }
+
+    public static function isToday($dateStr, $fromFormat = 'Y-m-d') {
+        $date = DateTime::createFromFormat($fromFormat, $dateStr);
+        $today = new DateTime("now");
+        if ($date == $today)
+            return true;
+
+        return false;
+    }
+
+}
